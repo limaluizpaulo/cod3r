9cb11b5586215e40b1d615476cca4bc39f420c79
Luiz Paulo Bernardo de Lima <limaluizpaulo@users.noreply.github.com>
2020-12-07 01:21:51 -0300

    primeiro exercicio

---
+++ exercicio01.js
@@ -0,0 +1,26 @@
+/* 
+Crie uma função que dado dois valores (passados como parâmetros) 
+mostre no console a soma, subtração,multiplicação e divisão desses valores.
+*/
+
+function getRandom(min = 0, max = 100) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
+
+let valor1 = getRandom()
+let valor2 = getRandom()
+
+operacoes = (a,b) => { 
+   return   `
+   valor 1 = ${valor1}
+   valor 2 = ${valor2}
+   soma: ${a + b} 
+   subtração: ${a - b} 
+   multiplicação: ${a * b} 
+   divisão: ${((a / b).toFixed(2))} 
+
+   `
+}
+console.log(operacoes(valor1,valor2))
