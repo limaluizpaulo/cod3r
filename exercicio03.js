83b6c343e9b25ce3ad0ede58735d786ff0e43b0b
Luiz Paulo Bernardo de Lima <limaluizpaulo@users.noreply.github.com>
2020-12-11 22:35:00 -0300

    novo exercicio

--- exercicio03.js
+++ exercicio03.js
@@ -1,3 +1,7 @@
+/* Crie uma função que recebe dois parâmetros, base e expoente, 
+e retorne a base elevada ao expoente*/
+
+
 function getRandom(min = 0, max = 10) {
     min = Math.ceil(min);
     max = Math.floor(max);
